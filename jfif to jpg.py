a343af7f07644abdf3c0ad83fd2f8bddbbd2c82f
Clarity <50315458+ClarityNZ@users.noreply.github.com>
2023-10-27 15:59:33 +1300

    Added statistics

--- jfif to jpg.py
+++ jfif to jpg.py
@@ -1,24 +1,31 @@
 from os import listdir, curdir, rename
 files = listdir(curdir)
+renamed_files = 0
+duped_files = 0
+
 for file in files:
     if '.jfif' in file:
         try:
             newfile = file.replace('.jfif', '.jpg')
             rename(file, newfile)
+            renamed_files += 1
             continue
         except:
             newfile = file.replace('.jfif', '_dupe.jpg')
             rename(file, newfile)
+            duped_files += 1
             continue
     elif '.jpg_large' in file:
         try:
             newfile = file.replace('.jpg_large','.jpg')
             rename(file, newfile)
+            renamed_files +=1
             continue
         except:
             newfile = file.replace('.jpg_large','_dupe.jpg')
             rename(file, newfile)
+            duped_files += 1
             continue
         
 
-input("Process completed, press enter to close")
+input("Successfully renamed {renamed_files} file/s and duped {duped_files}\npress enter to close.".format(renamed_files = renamed_files, duped_files = duped_files))
